5d82490df471a8624bbd9a675226763df28d8e60
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-08-02 21:21:35 -0500

    Fix class name

--- lib/data/datasources/remote/user_remote_datasource.dart
+++ lib/data/datasources/remote/user_remote_datasource.dart
@@ -7,7 +7,7 @@ import '../../../domain/entities/random_user.dart';
 import '../../models/random_user_json_response_model.dart';
 import '../../models/random_user_model.dart';
 
-class UserRemoteDatatasource {
+class UserRemoteDataSource {
   Future<RandomUser> getUser() async {
     //https://randomuser.me/api/?format=json&results=1
 
